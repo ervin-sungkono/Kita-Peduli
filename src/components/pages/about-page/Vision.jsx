2234ae699acde124c5bad86810494eef2bfca8eb
Ervin Cahyadinata Sungkono <ervinsung@gmail.com>
2022-08-30 00:29:44 +0700

    fix vision card animation and width issue

--- src/components/pages/about-page/Vision.jsx
+++ src/components/pages/about-page/Vision.jsx
@@ -3,43 +3,44 @@ import React, { useState } from "react";
 import BackgroundImage from "../../../assets/images/backgroundCard.png";
 
 const Vision = () => {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(0);
 
   const toggle = (i) => {
     if (selected === i) {
       return setSelected(null);
     }
-
     setSelected(i);
   };
 
   return (
-    <div className="vision-container">
-      <h2 className="vision-title">Visi Kami</h2>
-      <div className="vision-cards">
-        {data.map((items, i) => (
-          <div
-            className={selected === i ? "cardVision active-card" : "cardVision"}
-            onClick={() => toggle(i)}
-          >
-            <img className="cardImage" src={items.image} alt="" />
-            <h2
-              className={selected === i ? "cardNumber" : "cardNumber absolute"}
-            >
-              {items.number}
-            </h2>
+    <section id="vision-section">
+      <div className="container">
+        <h2 className="vision-title">Visi Kami</h2>
+        <div className="vision-cards">
+          {data.map((items, i) => (
             <div
-              className={
-                selected === i ? "cardContents show-detail" : "cardContents"
-              }
+              className={selected === i ? "cardVision active-card" : "cardVision"}
+              onClick={() => toggle(i)}
             >
-              <h4 className="cardSubTitle">{items.title}</h4>
-              <p className="cardCaption">{items.caption}</p>
+              <img className="cardImage" src={items.image} alt="" />
+              <h2
+                className={selected === i ? "cardNumber active-card" : "cardNumber"}
+              >
+                {items.number}
+              </h2>
+              <div
+                className={
+                  selected === i ? "cardContents show-detail" : "cardContents"
+                }
+              >
+                <p className="title">{items.title}</p>
+                <p className="cardCaption">{items.caption}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
