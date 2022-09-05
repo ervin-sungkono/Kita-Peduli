d8ed2daf6e1efbf7ef525ab744791685b6409efa
Ervin Cahyadinata Sungkono <ervinsung@gmail.com>
2022-09-05 14:56:14 +0700

    changed BrowserRouter to HashRouter for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './styles/main.scss';
 
